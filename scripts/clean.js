6d054116df2558f5bb2d8edec8d8c582ed25b3b3
Joost Ronkes Agerbeek <joost@basicbits.tech>
2022-10-24 21:57:24 +0200

    Empty buckets before deleting them.

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,4 +1,4 @@
-import { BucketsClient, DeleteBucketCommand } from "@stedi/sdk-client-buckets";
+import { BucketsClient, DeleteBucketCommand, ListObjectsCommand, DeleteObjectCommand } from "@stedi/sdk-client-buckets";
 import { FunctionsClient, DeleteFunctionCommand } from "@stedi/sdk-client-functions";
 import settings from "./settings.js"
 
@@ -19,14 +19,8 @@ async function main() {
 
   // Delete all resources in parallel.
   const results = await Promise.allSettled([
-    bucketsClient.send(new DeleteBucketCommand({
-      bucketName: settings.inputBucketName
-    })),
-
-    bucketsClient.send(new DeleteBucketCommand({
-      bucketName: settings.outputBucketName
-    })),
-
+    deleteBucket(bucketsClient, settings.inputBucketName),
+    deleteBucket(bucketsClient, settings.outputBucketName),
     functionsClient.send(new DeleteFunctionCommand({
       functionName: settings.functionName
     }))
@@ -36,11 +30,41 @@ async function main() {
   for (let result of results) {
     if (
       result.status === "rejected" &&
-      result.reason.name !== "ResourceNotFoundException"  // If the command failed because the
-                                                          // resource doesn’t exist, ignore it.
-                                                          // Gone is gone.
+      result.reason.name !== "ResourceNotFoundException" &&  // If the command failed because the
+      result.reason.name !== "NoSuchBucket"                  // resource doesn’t exist, ignore it.
+                                                             // Gone is gone.
     ) {
       console.error(result.reason);
     }
   }
+}
+
+async function deleteBucket(bucketsClient, bucketName) {
+  let pageToken = undefined;
+  do {
+    // Get a list of all the files in the bucket.
+    const listObjectsCommand = new ListObjectsCommand({
+      bucketName: bucketName,
+      pageToken: pageToken
+    });
+    const listObjectsResult = await bucketsClient.send(listObjectsCommand);
+
+    // Delete all the files in parallel.
+    if (listObjectsResult.items) {
+      await Promise.allSettled(
+        listObjectsResult.items.map(item => bucketsClient.send(new DeleteObjectCommand({
+          bucketName: bucketName,
+          key: item.key
+        })))
+      );
+    }
+
+    // The of files is paged, so go to the next page if necessary.
+    pageToken = listObjectsResult.nextPageToken;
+  } while (pageToken !== undefined);
+
+  // Delete the bucket.
+  await bucketsClient.send(new DeleteBucketCommand({
+    bucketName: bucketName
+  }));
 }
